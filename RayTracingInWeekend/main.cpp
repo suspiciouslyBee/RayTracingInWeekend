c4a2c6af3033800b31ab047d1176c2e5c08ae1d7
suspiciouslyBee <suspiciously@bee.movie>
2025-03-18 17:22:26 -0700

    checkpoint

--- RayTracingInWeekend/main.cpp
+++ RayTracingInWeekend/main.cpp
@@ -5,7 +5,11 @@
 #include "ray.h"
 
 //TODO: Why here?
-color ray_color(const ray& r) { return color(0, 0, 0); }
+color ray_color(const ray& r) { 
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
 
 int main() {
   int image_width = 400;
